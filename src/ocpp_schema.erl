6ab412d10e3368b0627c53918f27b71509b1f77a
Will Vining <wfv@vining.dev>
2023-02-25 07:59:46 -0700

    Simplify loading JSON schemas
    The jesse application provides a single function to load all schemas
    in a directory.

--- src/ocpp_schema.erl
+++ src/ocpp_schema.erl
@@ -7,22 +7,7 @@
 
 -spec init_schemas(SchemaDir :: file:name()) -> ok | {error, Reason :: any()}.
 init_schemas(SchemaDir) ->
-    case file:list_dir(SchemaDir) of
-        {ok, FileNames} ->
-            lists:foreach(
-              fun (FileName) -> load_schema(SchemaDir, FileName) end,
-              FileNames);
-        {error, Reason} -> {error, Reason}
-    end.
-
-load_schema(SchemaDir, FileName) ->
-    SchemaName = filename:rootname(filename:basename(FileName)),
-    Schema = read_schema(filename:join([SchemaDir, FileName])),
-    jesse:add_schema(SchemaName, Schema).
-
-read_schema(SchemaPath) ->
-    {ok, Data} = file:read_file(SchemaPath),
-    jiffy:decode(Data).
+    jesse:load_schemas(SchemaDir, fun jiffy:decode/1).
 
 -spec validate(ActionName :: binary(), Payload :: jesse:json_term()) ->
           ok |
