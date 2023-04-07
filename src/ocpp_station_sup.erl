12b119e0af7a4e86d468153b3ded74f0ce0cb967
Will Vining <wfv@vining.dev>
2023-04-07 14:33:59 -0600

    Revert "Start a new ocpp_station state machine for every connection"
    While this was a nice idea to simplify the occp_station state machine
    and message handling, it was ultimately a mistake. The OCPP
    requirements, specifically B04.FR.01 and B04.FR.02 preclude this
    because connection loss and resoration are not treated the same as
    rebooting a station. Only reboot of the station requires a new
    BootNotification Request.
    
    This reverts commit 23eef1572939cb422d4d1370403f3a705f107f92.

--- src/ocpp_station_sup.erl
+++ src/ocpp_station_sup.erl
@@ -7,26 +7,28 @@
 
 -behaviour(supervisor).
 
--export([start_link/0, start_station/1]).
+-export([start_link/0]).
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
 
+-define(SPEC(Station),
+        #{id => Station,
+          start => {ocpp_station, start_link, [Station]},
+          %% Should not restart on normal termination because stations
+          %% may be removed.
+          restart => transient,
+          type => worker,
+          modules => [ocpp_station]}).
+
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
--spec start_station(StationName :: binary()) -> supervisor:startchild_ret().
-start_station(StationName) ->
-    supervisor:start_child(?SERVER, [StationName, self()]).
-
 init([]) ->
-    SupFlags = #{strategy => simple_one_for_one,
-                 intensity => 10,
+    SupFlags = #{strategy => one_for_one,
+                 intensity => 1,
                  period => 3},
-    ChildSpec = #{id => station,
-                  start => {ocpp_station, start_link, []},
-                  %% don't want these to ever restart.
-                  restart => temporary,
-                  type => worker,
-                  modules => [ocpp_station]},
-    {ok, {SupFlags, [ChildSpec]}}.
+    {ok, {SupFlags, station_specs()}}.
+
+station_specs() ->
+    [?SPEC(Station) || Station <- ocpp_station_db:all_stations()].
