d7709f220fb49a653c698d5bae3fc1aecd065fea
Will Vining <wfv@vining.dev>
2023-06-02 15:04:35 -0600

    Remove Mnesia dependency
    This is no longer needed since separating the "frontend" websocket
    server from the core OCPP application.

--- src/ocpp.erl
+++ src/ocpp.erl
@@ -5,15 +5,30 @@
 
 -module(ocpp).
 
--export([install/1, add_charging_station/2]).
+-export([add_station/3]).
 
--spec install(Nodes :: [node()]) -> ok.
-install(Nodes) ->
-    ok = mnesia:create_schema(Nodes),
-    application:start(mnesia),
-    ocpp_station_db:install(Nodes),
-    ocpp_authentication:install(Nodes),
-    application:stop(mnesia).
+%% @doc Add a station to the CSMS. If succesful the Pid of the Station
+%% Manager process is returned. No action will be taken if a station
+%% with the same `StationId' has already been added. If the `Handler'
+%% returns an error from or fails in its `init/1' callback the value
+%% `{error, {init, Reason}}' is returned where `Reason' is the error
+%% reason from `HandlerModule:init/1'.
+%%
+%% @param StationId the name of the station. Must be unique.
+%% @param NumEVSE the number of EVSE connected to this station.
+%% @param Handler a tuple specifying the callback module that will
+%%        handler OCPP requests for the CSMS and the argument passed
+%%        to its `init/1` callback.
+%%
+%% @end
+-spec add_station(StationId :: binary(),
+                  NumEVSE :: pos_integer(),
+                  Handler :: {module(), any()}) ->
+          {ok, StationManager :: pid()} | {error, Reason}
+              when Reason :: already_exists
+                           | {init, any()}.
+add_station(StationId, NumEVSE, Handler) ->
+    ocpp_manager:add_station(StationId, NumEVSE, Handler).
 
 -spec add_charging_station(Id :: binary(), NumEvse :: pos_integer()) ->
           {ok, Password :: binary()} | {error, Reason :: any()}.
